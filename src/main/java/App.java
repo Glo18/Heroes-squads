813c302961991ad1c523b4bf9a2d2a5c8001a326
Glo18 <kiogoraspecial@gmail.com>
2020-12-01 13:11:34 +0300

    create success file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,10 +15,23 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/form", ((request, response) -> {
+        get("/heroform", ((request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "heroform.hbs");
         }), new HandlebarsTemplateEngine());
+
+        post("addHero", ((request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+
+            Hero hero = new Hero(name, age, power, weakness);
+
+            model.put("hero", hero);
+            return new ModelAndView(model, "successHero.hbs");
+            }, new HandlebarsTemplateEngine());
 
     }
 }

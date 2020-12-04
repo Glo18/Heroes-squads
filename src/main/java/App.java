6492694a23ba382a3d2b84383e01c995657a392f
Glo18 <kiogoraspecial@gmail.com>
2020-12-04 05:00:09 +0300

    update hero form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,8 @@ import models.Hero;
 import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import static spark.Spark.*;
@@ -32,23 +34,24 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
 
-        post("/addHero", (request, response) -> {
+        post("/new/hero", (request, response) -> {
 
             Map<String, Object> model = new HashMap<>();
             String name = request.queryParams("name");
             int age = Integer.parseInt(request.queryParams("age"));
             String power = request.queryParams("power");
             String weakness = request.queryParams("weakness");
-
-            Hero hero = new Hero(name, age, power, weakness);
-
-            model.put("hero", hero);
+            Hero newHero = new Hero(name,age,power,weakness);
+            request.session().attribute("item",name);
+            model.put("item",request.session().attribute("item"));
+            model.put("newHero",newHero);
             return new ModelAndView(model, "successhero.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/hero", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("addHeroes", Hero.getAll()); //displays hero
+            ArrayList<Hero> hero = (ArrayList<Hero>) Hero.getAll();//displays hero
+            model.put("hero",hero);
             return new ModelAndView(model, "hero.hbs");
         }, new HandlebarsTemplateEngine());
 
